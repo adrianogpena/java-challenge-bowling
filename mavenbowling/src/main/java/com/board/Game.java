673470a4d1dd1cb4678270660299bbe6ad965025
Adriano Guareschi <adrianogpena@gmail.com>
2021-03-29 13:15:48 -0300

    optimize score calculation

--- mavenbowling/src/main/java/com/board/Game.java
+++ mavenbowling/src/main/java/com/board/Game.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -42,17 +43,13 @@ public class Game {
           pinsKnockedDown = Integer.parseInt(line.split(" ")[1]);
         }
 
-        // System.out.println(playerName + " Turn: Pins " + pinsKnockedDown);
-
         if (!playerMap.containsKey(playerName)) {
-          // System.out.println("New player on the board!");
           playerMap.put(playerName, maxPlayerId);
           players.add(new Player(playerName));
           maxPlayerId++;
         }
 
         Player player = getPlayer(playerName);
-        // System.out.println("Player " + player.getName() + " selected!");
         
         player.roll(pinsKnockedDown, foul);
 
@@ -67,69 +64,59 @@ public class Game {
   public void calcScore() {
     for (String name : playerMap.keySet()) {
       Player player = getPlayer(name);
+      List<Frame> frames = player.getFrames();
       Frame frame;
       int score = 0;
+      int size = frames.size();
 
-      // Calc frames from 1 to 8
-      for (int i = 0; i < Player.MAX_FRAMES - 2; i++) {
+      for (int i = 0; i < size; i++) {
         frame = player.getFrame(i);
         score += frame.getRoll(0) + frame.getRoll(1);
 
         if (frame.getIsSpare()) {
-          score += player.getFrame(i + 1).getRoll(0);
+          switch (i) {
+            case 9:
+              score += frame.getRoll(2);
+              break;
+
+            default:
+              score += player.getFrame(i + 1).getRoll(0);
+              break;
+          }
         }
 
         if (frame.getIsStrike()) {
-          if (player.getFrame(i + 1).getIsStrike()) {
-            score += player.getFrame(i + 1).getRoll(0) + player.getFrame(i + 2).getRoll(0);
-          } else {
-            score += player.getFrame(i + 1).getRoll(0) + player.getFrame(i + 1).getRoll(1);
+          switch (i) {
+            case 8:
+              score += player.getFrame(i + 1).getRoll(0) + player.getFrame(i + 1).getRoll(1);
+              break;
+
+            case 9:
+              score += frame.getRoll(2);
+              break;
+
+            default:
+              if (player.getFrame(i + 1).getIsStrike()) {
+                score += player.getFrame(i + 1).getRoll(0) + player.getFrame(i + 2).getRoll(0);
+              } else {
+                score += player.getFrame(i + 1).getRoll(0) + player.getFrame(i + 1).getRoll(1);
+              }
+              break;
           }
         }
 
         frame.setScore(score);
       }
-
-      // Calc frame 9
-      frame = player.getFrame(Player.MAX_FRAMES - 2);
-      score += frame.getRoll(0) + frame.getRoll(1);
-
-      if (frame.getIsSpare()) {
-        score += player.getFrame(Player.MAX_FRAMES - 1).getRoll(0);
-      }
-
-      if (frame.getIsStrike()) {
-        score += player.getFrame(Player.MAX_FRAMES - 1).getRoll(0) + player.getFrame(Player.MAX_FRAMES - 1).getRoll(1);
-      }
-
-      frame.setScore(score);
-
-      // Calc fram 10
-      frame = player.getFrame(Player.MAX_FRAMES - 1);
-      score += frame.getRoll(0) + frame.getRoll(1);
-
-      if (frame.getIsSpare() || frame.getIsStrike()) {
-        score += frame.getRoll(2);
-      }
-
-      frame.setScore(score);
     }
   }
 
   public void printPlayers() {
     StringBuilder scoreBoard = new StringBuilder();
 
-    scoreBoard.append(StringUtils.rightPad("Frame", 10))
-              .append(StringUtils.rightPad("1", 6))
-              .append(StringUtils.rightPad("2", 6))
-              .append(StringUtils.rightPad("3", 6))
-              .append(StringUtils.rightPad("4", 6))
-              .append(StringUtils.rightPad("5", 6))
-              .append(StringUtils.rightPad("6", 6))
-              .append(StringUtils.rightPad("7", 6))
-              .append(StringUtils.rightPad("8", 6))
-              .append(StringUtils.rightPad("9", 6))
-              .append("10").append('\n');
+    scoreBoard.append(StringUtils.rightPad("Frame", 10));
+
+    IntStream.range(1, 11).forEach(frameNumber -> scoreBoard.append(StringUtils.rightPad(String.valueOf(frameNumber), 6)));
+    scoreBoard.append('\n');
 
     players.stream().forEach(player -> scoreBoard.append(player));
 
